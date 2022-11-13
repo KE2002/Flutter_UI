41ce55ef8119dc27ab16f6dc86594883ccc5184a
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-11-13 13:51:54 +0530

    Hii

--- lib/gender.dart
+++ lib/gender.dart
@@ -12,128 +12,130 @@ class Gender extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: primaryColor,
-      body: SizedBox(
-        width: size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: primaryColor,
-              ),
-              child: Image.asset("assets/images/couple.png"),
-            ),
-            Container(
-              width: size.width * 0.85,
-              padding: const EdgeInsets.all(25),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: primaryColor,
                 ),
+                child: Image.asset("assets/images/couple.png"),
               ),
-              child: Column(
-                children: <Widget> [
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: const TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Look Good, Feel Good\n",
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Color(0xFF1D1E20),
+              Container(
+                width: size.width * 0.85,
+                padding: const EdgeInsets.all(25),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget> [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Look Good, Feel Good\n",
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Color(0xFF1D1E20),
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                                "\nCreate your individual & unique style and look amazing everyday.",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color(0xFF8F959E),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Gap(20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget> [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const WomenSection(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            backgroundColor: const Color(0xFFF5F6FA),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.only(
+                              top: 12,
+                              left: 16,
+                              bottom: 12,
+                              right: 16,
+                            ),
+                            child: Text(
+                              "Women",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFF8F959E),
+                              ),
+                            ),
                           ),
                         ),
-                        TextSpan(
-                          text:
-                              "\nCreate your individual & unique style and look amazing everyday.",
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Color(0xFF8F959E),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MenSection(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.only(
+                              top: 12,
+                              left: 24,
+                              bottom: 12,
+                              right: 24,
+                            ),
+                            child: Text(
+                              "Men",
+                              style: TextStyle(fontSize: 20),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  const Gap(20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget> [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const WomenSection(),
-                            ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          backgroundColor: const Color(0xFFF5F6FA),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                            top: 12,
-                            left: 16,
-                            bottom: 12,
-                            right: 16,
-                          ),
-                          child: Text(
-                            "Women",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Color(0xFF8F959E),
-                            ),
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const MenSection(),
-                            ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                            top: 12,
-                            left: 24,
-                            bottom: 12,
-                            right: 24,
-                          ),
-                          child: Text(
-                            "Men",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Gap(20),
-                  GestureDetector(
-                    child: const Text("Skip"),
-                  )
-                ],
+                    const Gap(20),
+                    GestureDetector(
+                      child: const Text("Skip"),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/signin.dart
+++ lib/signin.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:login/constant.dart';
 import 'package:login/gender.dart';
-import 'package:login/login.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
@@ -19,101 +18,103 @@ class SignIn extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
-            const Gap(20),
-            const Text(
-              "Welcome Back",
-              style: TextStyle(color: textColor, fontSize: 32),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam  etcc ",
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              const Gap(20),
+              const Text(
+                "Welcome Back",
+                style: TextStyle(color: textColor, fontSize: 32),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: textBoxColor,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide:
-                        const BorderSide(color: textBoxColor, width: 0.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide:
-                        const BorderSide(color: textBoxColor, width: 0.0),
-                  ),
-                  hintText: "Username",
-                  hintStyle: TextStyle(
-                    color: primaryColor.withOpacity(0.5),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: textBoxColor,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide:
-                        const BorderSide(color: textBoxColor, width: 0.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide:
-                        const BorderSide(color: textBoxColor, width: 0.0),
-                  ),
-                  hintText: "Password",
-                  hintStyle: TextStyle(
-                    color: primaryColor.withOpacity(0.5),
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: const Text("Forgot Password?"),
-            ),
-            const Gap(30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Gender(),
-                    ),
-                    (Route route) => false);
-              },
-              style: ElevatedButton.styleFrom(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
+              const Padding(
+                padding: EdgeInsets.all(30.0),
                 child: Text(
-                  "Enter",
-                  style: TextStyle(fontSize: 20),
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam  etcc ",
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: textBoxColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          const BorderSide(color: textBoxColor, width: 0.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          const BorderSide(color: textBoxColor, width: 0.0),
+                    ),
+                    hintText: "Username",
+                    hintStyle: TextStyle(
+                      color: primaryColor.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: textBoxColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          const BorderSide(color: textBoxColor, width: 0.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide:
+                          const BorderSide(color: textBoxColor, width: 0.0),
+                    ),
+                    hintText: "Password",
+                    hintStyle: TextStyle(
+                      color: primaryColor.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const Text("Forgot Password?"),
+              ),
+              const Gap(30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Gender(),
+                      ),
+                      (Route route) => false);
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "Enter",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
