394493d1f79ce26a46ccd16fec09e15f3e6ac527
KE2002 <44949462+KE2002@users.noreply.github.com>
2022-11-14 16:22:52 +0530

    pusheses

--- lib/save_card.dart
+++ lib/save_card.dart
@@ -1,36 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:login/choosebrand.dart';
 
 import 'brand_card.dart';
 import 'constant.dart';
 
-class SaveCard extends StatelessWidget {
+class SaveCard extends StatefulWidget {
   const SaveCard({super.key});
 
+  @override
+  State<SaveCard> createState() => _SaveCardState();
+}
+
+class _SaveCardState extends State<SaveCard> {
+  bool isstate = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xFFF3F3F3),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.only(left: 20 * 2, right: 20 * 2),
-        height: 80,
-        decoration: BoxDecoration(
-          color: primaryColor,
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(0, -10),
-              blurRadius: 35,
-              color: primaryColor.withOpacity(0.40),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const SaveCard(),
             ),
-          ],
-        ),
-        child: const Center(
-          child: Text(
-            "Save Address",
-            style: TextStyle(
-                fontWeight: FontWeight.w500, fontSize: 17, color: Colors.white),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(18),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, -10),
+                blurRadius: 35,
+                color: primaryColor.withOpacity(0.40),
+              ),
+            ],
+            color: primaryColor,
+          ),
+          width: size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Save Card",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFFFEFEFE),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -109,7 +131,204 @@ class SaveCard extends StatelessWidget {
               ],
             ),
           ),
-          ElevatedButton(onPressed: () {}, child: Text("Add new Card"))
+          const Gap(15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 20, bottom: 20),
+                  backgroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                child: SizedBox(
+                  width: size.width - 50,
+                  child: const Text(
+                    'Add new Card',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Gap(15),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
+            child: Row(
+              children: const [
+                Text(
+                  "Card Owner",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: TextField(
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFF5F6FA),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
+                ),
+                hintText: "Adam",
+                hintStyle: TextStyle(
+                  color: primaryColor.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+          const Gap(15),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
+            child: Row(
+              children: const [
+                Text(
+                  "Card Number",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: TextField(
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFF5F6FA),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
+                ),
+                hintText: "5254 7634 8734 7690",
+                hintStyle: TextStyle(
+                  color: primaryColor.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+          const Gap(15),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "EXP",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 17),
+                      ),
+                      const Gap(5),
+                      TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFF5F6FA),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(
+                                color: textBoxColor, width: 0.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(
+                                color: textBoxColor, width: 0.0),
+                          ),
+                          hintText: "24/24",
+                          hintStyle: TextStyle(
+                            color: primaryColor.withOpacity(0.5),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(20),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "CVV",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 17),
+                      ),
+                      const Gap(5),
+                      TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFF5F6FA),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(
+                                color: textBoxColor, width: 0.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            borderSide: const BorderSide(
+                                color: textBoxColor, width: 0.0),
+                          ),
+                          hintText: "7475",
+                          hintStyle: TextStyle(
+                            color: primaryColor.withOpacity(0.5),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Save as primary address",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+                ),
+                CupertinoSwitch(
+                  value: isstate,
+                  onChanged: (value) {
+                    isstate = value;
+                    setState(
+                      () {},
+                    );
+                  },
+                  thumbColor: CupertinoColors.white,
+                  activeColor: primaryColor,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
