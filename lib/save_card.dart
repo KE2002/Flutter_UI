5d4af4a7117b2267a61818855d24882125f6754c
KE2002 <44949462+KE2002@users.noreply.github.com>
2022-11-14 16:27:17 +0530

    gola

--- lib/save_card.dart
+++ lib/save_card.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-import 'brand_card.dart';
 import 'constant.dart';
 
 class SaveCard extends StatefulWidget {
@@ -18,7 +17,7 @@ class _SaveCardState extends State<SaveCard> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Color(0xFFF3F3F3),
+      backgroundColor: const Color(0xFFF3F3F3),
       bottomNavigationBar: GestureDetector(
         onTap: () {
           Navigator.push(
@@ -92,244 +91,250 @@ class _SaveCardState extends State<SaveCard> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/images/Card1.png",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/images/Card2.png",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Gap(15),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Image.asset(
-                          "assets/images/Card1.png",
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    padding: const EdgeInsets.only(
+                        left: 10, right: 10, top: 20, bottom: 20),
+                    backgroundColor: Colors.white,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: size.width - 50,
+                    child: const Text(
+                      'Add new Card',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: primaryColor,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Gap(15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
+              child: Row(
+                children: const [
+                  Text(
+                    "Card Owner",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: TextField(
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color(0xFFF5F6FA),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide:
+                        const BorderSide(color: textBoxColor, width: 0.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide:
+                        const BorderSide(color: textBoxColor, width: 0.0),
+                  ),
+                  hintText: "Adam",
+                  hintStyle: TextStyle(
+                    color: primaryColor.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            ),
+            const Gap(15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
+              child: Row(
+                children: const [
+                  Text(
+                    "Card Number",
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: TextField(
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: const Color(0xFFF5F6FA),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide:
+                        const BorderSide(color: textBoxColor, width: 0.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide:
+                        const BorderSide(color: textBoxColor, width: 0.0),
+                  ),
+                  hintText: "5254 7634 8734 7690",
+                  hintStyle: TextStyle(
+                    color: primaryColor.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            ),
+            const Gap(15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "EXP",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 17),
+                        ),
+                        const Gap(5),
+                        TextField(
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color(0xFFF5F6FA),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(
+                                  color: textBoxColor, width: 0.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(
+                                  color: textBoxColor, width: 0.0),
+                            ),
+                            hintText: "24/24",
+                            hintStyle: TextStyle(
+                              color: primaryColor.withOpacity(0.5),
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Image.asset(
-                          "assets/images/Card2.png",
+                  const Gap(20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "CVV",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 17),
+                        ),
+                        const Gap(5),
+                        TextField(
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color(0xFFF5F6FA),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(
+                                  color: textBoxColor, width: 0.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(
+                                  color: textBoxColor, width: 0.0),
+                            ),
+                            hintText: "7475",
+                            hintStyle: TextStyle(
+                              color: primaryColor.withOpacity(0.5),
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const Gap(15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  padding: const EdgeInsets.only(
-                      left: 10, right: 10, top: 20, bottom: 20),
-                  backgroundColor: Colors.white,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                ),
-                child: SizedBox(
-                  width: size.width - 50,
-                  child: const Text(
-                    'Add new Card',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: primaryColor,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Gap(15),
-          Padding(
-            padding: EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
-            child: Row(
-              children: const [
-                Text(
-                  "Card Owner",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15),
-            child: TextField(
-              onChanged: (value) {},
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFFF5F6FA),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
-                ),
-                hintText: "Adam",
-                hintStyle: TextStyle(
-                  color: primaryColor.withOpacity(0.5),
-                ),
+                  )
+                ],
               ),
             ),
-          ),
-          const Gap(15),
-          Padding(
-            padding: EdgeInsets.only(left: 15.0, right: 15, bottom: 10),
-            child: Row(
-              children: const [
-                Text(
-                  "Card Number",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15),
-            child: TextField(
-              onChanged: (value) {},
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFFF5F6FA),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: const BorderSide(color: textBoxColor, width: 0.0),
-                ),
-                hintText: "5254 7634 8734 7690",
-                hintStyle: TextStyle(
-                  color: primaryColor.withOpacity(0.5),
-                ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Save as primary address",
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+                  ),
+                  CupertinoSwitch(
+                    value: isstate,
+                    onChanged: (value) {
+                      isstate = value;
+                      setState(
+                        () {},
+                      );
+                    },
+                    thumbColor: CupertinoColors.white,
+                    activeColor: primaryColor,
+                  ),
+                ],
               ),
-            ),
-          ),
-          const Gap(15),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "EXP",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 17),
-                      ),
-                      const Gap(5),
-                      TextField(
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Color(0xFFF5F6FA),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: const BorderSide(
-                                color: textBoxColor, width: 0.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: const BorderSide(
-                                color: textBoxColor, width: 0.0),
-                          ),
-                          hintText: "24/24",
-                          hintStyle: TextStyle(
-                            color: primaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Gap(20),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "CVV",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 17),
-                      ),
-                      const Gap(5),
-                      TextField(
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Color(0xFFF5F6FA),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: const BorderSide(
-                                color: textBoxColor, width: 0.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            borderSide: const BorderSide(
-                                color: textBoxColor, width: 0.0),
-                          ),
-                          hintText: "7475",
-                          hintStyle: TextStyle(
-                            color: primaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Save as primary address",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
-                ),
-                CupertinoSwitch(
-                  value: isstate,
-                  onChanged: (value) {
-                    isstate = value;
-                    setState(
-                      () {},
-                    );
-                  },
-                  thumbColor: CupertinoColors.white,
-                  activeColor: primaryColor,
-                ),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
